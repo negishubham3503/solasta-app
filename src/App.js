5c7a97c2ee9ee1943763c956e9f5e7410a5a5825
anshuman2051 <anshumansingh2051@gmail.com>
2019-02-02 03:55:18 +0530

    added another component for events details modified component structure for particles component (renamed particle.jsx from title.jsx)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import Title from './components/header/Title';
+import NavBar from './components/header/NavBar';
 import Footer from './components/footer/Footer';
+import Body from './components/body/Body';
+import Events from './components/events/events';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Title/>
+        <NavBar/>
+        {/* <Body/> */}
+        <Events/>
         <Footer/>
       </React.Fragment>
     );
--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -3,19 +3,19 @@ import React, { Component } from 'react';
 class NavBar extends Component {
     render() { 
         return ( 
-            <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-                <a className="navbar-brand b-1" href="#">SOLASTA</a>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top border">
+                <a className="navbar-brand b-1" href=".particles">SOLASTA</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent" >
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" href="#info1">Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
+                            <a className="nav-link" href="#info2">Link</a>
                         </li>
 
                     </ul>
