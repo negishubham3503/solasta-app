d99c9ce55af86a09c58a33c94ef876742c568ec4
anshuman2051 <anshumansingh2051@gmail.com>
2019-02-08 18:15:30 +0530

    modified sidenav for events category

--- src/components/body/events/events.jsx
+++ src/components/body/events/events.jsx
@@ -1,9 +1,12 @@
+//todo hiding small right angle bracket when toggling
 import React, { Component } from 'react';
 import Card from './eventsCard';
+import './events.css';
 
 class Events extends Component {
     state = {
         events : ['a','b','c','d','e'],
+        cat_window_status: true,
         event_details : {
             art : {
                 desc : ' event about art',
@@ -50,12 +53,14 @@ class Events extends Component {
     };
     render() { 
         return ( 
-                <div className="container-fluid row mx-0 px-0 " >
+                <div className="container-fluid row mx-0 px-0 " style={{position:'relative'}}>
+                    {/* icon for opening sidenav on small screen */}
+                    <span className='fa fa-lg fa-angle-right ' style={{position:'absolute',top:'5%',left:'5%',zIndex:'1'}} onClick={this.toggleWindow}/>
 
                     {/*side nav for choosing event type  */}
-                    <div className="col-md-3 col-lg-2 shadow-lg border" style={{marginTop:"40px",backgroundColor:"#3f51b5"}}>
+                    <div className="col-md-3 col-lg-2 shadow-lg border " id='category-chooser' style={{marginTop:"40px",backgroundColor:"#3f51b5"}}>
                         <h2 className="text-light mt-5 mb-3  mx-0">Category 
-                            <span className="fa fa-xs fa-close text-light float-right mt-1" style={{cursor:"pointer"}}/>
+                            <span className="fa fa-xs fa-close text-light float-right mt-1" style={{cursor:"pointer"}} onClick={this.toggleWindow}/>
                         </h2>
                         <ul className="text-light" style={{listStyleType:'none'}}>
                             {this.renderEventCategory()}
@@ -81,6 +86,21 @@ class Events extends Component {
                 </li> ))}
             </ul>
         );
+    }
+
+    toggleWindow = ()=>{
+        this.setState({cat_window_status: !this.state.cat_window_status});
+
+         // hides cat window for size less than 768px
+        let element = document.getElementById('category-chooser');
+        if(window.screen.availWidth <= 768 ){
+            if(this.state.cat_window_status){
+                element.style['display']='block';
+            }
+            else{
+                element.style['display']='none';
+            }
+        }
     }
 }
  
